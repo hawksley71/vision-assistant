159684462766ec91e43e8367ae9e5bda2a9f3e88
hawksley71 <hawksley71@gmail.com>
2025-05-13 09:55:25 -0400

    Remove archive and deprecated folders from git tracking and add to gitignore

--- archive/gTTS_test.py
+++
@@ -1,7 +0,0 @@
-from gtts import gTTS
-import os
-
-text = "In the past hour, I saw a person and a cat."
-tts = gTTS(text)
-tts.save("output.mp3")
-os.system("mpv output.mp3")  # or "vlc", "aplay", etc.
